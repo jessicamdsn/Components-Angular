d82ff258e6aa76147ee14e5844556cc08247ae57
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-07-18 15:42:37 -0300

    rename

--- carrossel/src/app/components/carrossel/carrossel.component.ts
+++ carrossel/src/app/components/carrossel/carrossel.component.ts
@@ -10,6 +10,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CarrosselComponent {
+  
   slides = [
     {
       name: 'Rosa claro',
